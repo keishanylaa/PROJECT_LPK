56a38223df887156447092d902e1540bc58c2b12
keishanylaa <keishanyla@gmail.com>
2025-12-11 15:41:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,4 @@ st.number_input(label, min_value=0, max_value=100000,
 if number%2==1:
   st.write("Bilangan",number,"termasuk bilangan ganjil")
 else:
-st.write("Bilangan",number,"termasuk bilangan genap")
+  st.write("Bilangan",number,"termasuk bilangan genap")
